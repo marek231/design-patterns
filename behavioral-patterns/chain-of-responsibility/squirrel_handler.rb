b3014868a79a328a6998aac52612a85800a41b0d
marek231 <pruteanupopescumarek@gmail.com>
2019-08-27 12:26:18 +0300

    Implement Chain of Responsibility pattern

---
+++ behavioral-patterns/chain-of-responsibility/squirrel_handler.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'abstract_handler'
+
+class SquirrelHandler < AbstractHandler
+  def handle(request)
+    if request == 'Nut'
+      "Squirrel: I'll eat the #{request}"
+    else
+      super(request)
+    end
+  end
+end
